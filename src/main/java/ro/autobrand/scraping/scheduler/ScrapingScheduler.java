856dbecafd9b4d3439ccd2e9d5eed1158c17701e
Rares Baciu <rares@Raress-MacBook-Air.local>
2026-05-22 10:42:41 +0300

    feat: schedule hourly scraping between 12:00-18:00

---
+++ src/main/java/ro/autobrand/scraping/scheduler/ScrapingScheduler.java
@@ -0,0 +1,26 @@
+package ro.autobrand.scraping.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import ro.autobrand.scraping.service.ScrapingService;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ScrapingScheduler {
+
+    private final ScrapingService scrapingService;
+
+    @Scheduled(cron = "0 0 12-18 * * *", zone = "Europe/Bucharest")
+    public void scheduledScrape() {
+        log.info("Cron-triggered scraping run starting");
+        try {
+            int count = scrapingService.runScraping();
+            log.info("Cron-triggered scraping run finished: {} product(s) processed", count);
+        } catch (Exception ex) {
+            log.error("Cron-triggered scraping run failed", ex);
+        }
+    }
+}
